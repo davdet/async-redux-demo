33a02fe7b734ae158c06c7702f40a1a3a62cfda2
davdet <dv.dt@hotmail.com>
2021-07-28 10:11:00 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# async-redux-demo
+
+Example of using Redux with asynchronous code.
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import Cart from "./components/Cart/Cart";
+import Layout from "./components/Layout/Layout";
+import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { sendCartData, fetchCartData } from "./store/cart-actions";
+
+let isInitial = true;
+
+function App() {
+  const dispatch = useDispatch();
+
+  const showCart = useSelector((state) => {
+    return state.ui.cartIsVisible;
+  });
+  const cart = useSelector((state) => {
+    return state.cart;
+  });
+  const notification = useSelector((state) => {
+    return state.ui.notification;
+  });
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
+  );
+}
+
+export default App;
